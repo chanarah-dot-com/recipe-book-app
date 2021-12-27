344913341f06b5a03ed3a54618e838593f33f1c9
Carl 'Cid' Romero <carlcidromero@gmail.com>
2021-12-27 14:17:58 -0800

    Feature: Add search field

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ function App() {
     <div className='App'>
       <div id='top'></div>
       <h1>She Chefs Recipes</h1>
+      <div>
+        <label htmlFor='search'>Search: </label>
+        <input
+          type='text'
+          name='search'
+          id='search'
+          placeholder='Find a recipe...'
+        />
+      </div>
       <div
         style={{
           width: '80%',
